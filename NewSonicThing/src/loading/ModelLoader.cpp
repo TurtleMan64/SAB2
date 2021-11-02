63abaa519c60779fa7deddcd5958e6025700156b
TurtleMan64 <turtleboy1026@aol.com>
2021-11-02 14:42:03 -0700

    Cleaned up ModelLoaver

--- NewSonicThing/src/loading/ModelLoader.cpp
+++ NewSonicThing/src/loading/ModelLoader.cpp
@@ -12,7 +12,6 @@
 #include "../textures/modeltexture.hpp"
 #include "../loading/loader.hpp"
 #include "../toolbox/vector.hpp"
-#include "vertex.hpp"
 #include "../engineTester/main.hpp"
 #include "../toolbox/split.hpp"
 #include "../toolbox/getline.hpp"
@@ -20,6 +19,47 @@
 #include "../collision/triangle3d.hpp"
 #include "../collision/quadtreenode.hpp"
 #include "../toolbox/maths.hpp"
+
+ModelLoader::Vertex::Vertex(int index, Vector3f* position, Vector4f* color)
+{
+    this->index = index;
+    this->color.set(color);
+    this->position.set(position->x, position->y, position->z);
+    this->length = position->length();
+    this->textureIndex = NO_INDEX;
+    this->normalIndex = NO_INDEX;
+    this->duplicateVertex = nullptr;
+}
+
+ModelLoader::Vertex::Vertex(int index, Vector3f* position)
+{
+    this->index = index;
+    //this->color.set(Maths::nextUniform(), Maths::nextUniform(), Maths::nextUniform());
+    this->color.set(1.0f, 1.0f, 1.0f, 1.0f);
+    this->position.set(position->x, position->y, position->z);
+    this->length = position->length();
+    this->textureIndex = NO_INDEX;
+    this->normalIndex = NO_INDEX;
+    this->duplicateVertex = nullptr;
+}
+
+int ModelLoader::Vertex::isSet()
+{
+    if ((textureIndex != NO_INDEX) && (normalIndex != NO_INDEX))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int ModelLoader::Vertex::hasSameTextureAndNormal(int textureIndexOther, int normalIndexOther)
+{
+    if ((textureIndexOther == textureIndex) && (normalIndexOther == normalIndex))
+    {
+        return 1;
+    }
+    return 0;
+}
 
 ModelLoader::FakeTexture::FakeTexture()
 {
